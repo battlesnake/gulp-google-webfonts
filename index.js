battlesnake/gulp-google-webfonts

commit b3f9818d292b906da60ceebcc161582c119c6ae8

Improved code quality of contributions, defined fonts.list format a bit better

index.js
@@ -5,28 +5,18 @@ var http = require('http');
 var File = require('vinyl');
 var path = require('path');
 var async = require('async');
+var _ = { defaults: require('lodash.defaults') };
 
 module.exports = getter;
 
+var defaultOptions = {
+	cssFilename: 'fonts.css',
+	fontsDir: './',
+	cssDir: './'
+};
+
 function getter(options) {
-	if (!options) {
-		options = {};
-	}
-	if (!options.cssFilename) {
-		options.cssFilename = 'fonts.css';
-	}
-	if (!options.fontsDir) {
-		options.fontsDir = './';
-	}
-	if (!options.cssDir) {
-		options.cssDir = './';
-	}
-	if (options.fontsDir.substr(options.fontsDir.length - 1) !== '/') {
-		options.fontsDir += '/';
-	}
-	if (options.cssDir.substr(options.cssDir.length - 1) !== '/') {
-		options.cssDir += '/';
-	}
+	options = _.defaults({}, options, defaultOptions);
 	return through.obj(processor);
 
 	function processor(file, enc, next) {
@@ -37,17 +27,64 @@ function getter(options) {
 		if (file.isStream()) {
 			return self.emit('error', new Error('webfont-getter: Streaming not supported'));
 		}
-		var param = file.contents.toString(enc).split('\n').join('|').replace(/^\|*|\|*$/g, '');
+		var subsets = [];
+		var param = file.contents
+			.toString(enc)
+			.split('\n')
+			.map(function (s) { return s.trim(); })
+			.filter(function (s) { return s.length > 0 && s.charAt(0) !== '#'; })
+			.map(parseLine)
+			.join('|')
+			.replace(/^\|*|\|*$/g, '');
 		async.waterfall([initial(param), requestCss, receiveCss, parseCss, downloadFonts], next);
+
+		function parseLine(line) {
+			if (line.indexOf('\t') === -1) {
+				/* Extract subsets if specified */
+				var ss = line.match(/&subset=.*$/);
+				if (ss) {
+					line = line.substr(0, ss.index);
+					addSubsets(ss[0].substr(8).split(','));
+				}
+				return line.replace(/ /g, '+');
+			} else {
+				return parseTabDelimetedLine(line);
+			}
+		}
+
+		function parseTabDelimetedLine(line) {
+			var fields = line.split('\t');
+			var face = fields[0];
+			var style = fields[1] || '400';
+			var subset = fields[2];
+			if (subset) {
+				addSubsets(subset.split(','));
+			}
+			return face + ':' + style;
+		}
+
+		function addSubsets(s) {
+			if (!s || !s.length) {
+				return;
+			}
+			s.forEach(function (subset) {
+				if (subsets.indexOf(subset) === -1) {
+					subsets.push(subset);
+				}
+			});
+		}
 
 		function initial(value) {
 			return function (next) { return next(null, value); };
 		}
 
 		function requestCss(param, next) {
+			if (subsets.length) {
+				param = param + '&subset=' + subsets.join(',');
+			}
 			var req = {
 				host: 'fonts.googleapis.com',
-				path: '/css?family=' + encodeURIComponent(param),
+				path: '/css?family=' + param,
 				headers: {
 					'User-Agent': 'Mozilla/4.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36'
 				}
@@ -60,11 +97,15 @@ function getter(options) {
 		function receiveCss(res, next) {
 			var css = [];
 			res.on('data', function (data) { css.push(data.toString()); });
+			res.on('error', next);
 			res.on('end', function () { next(null, css.join('')); });
 		}
 
 		function parseCss(css, next) {
 			css = css.replace(/\s*([{}:;\(\)])\s*/g, '$1');
+			if (css.substr(0, 2) === '<!') {
+				return next(new Error('Failed to retrieve webfont CSS'));
+			}
 			var rx = /@font-face{font-family:'([^']+)';font-style:(\w+);font-weight:(\w+);src:[^;]*url\(([^)]+\.woff)\)[^;]*;}/g;
 			var requests = [];
 			css.replace(rx, function (block, family, style, weight, url) {
@@ -81,18 +122,19 @@ function getter(options) {
 				'	font-family: \'$family\';',
 				'	font-style: $style;',
 				'	font-weight: $weight;',
-				'	src: url(' + options.fontsDir + '$name) format(\'woff\');',
+				'	src: url($name) format(\'woff\');',
 				'}'
 			].join('\n');
 			var css = requests
 				.map(makeFontFace)
 				.join('\n\n');
 			self.push(new File({
-				path: options.cssDir + options.cssFilename,
+				path: path.join(options.cssDir, options.cssFilename),
 				contents: new Buffer(css, 'utf8')
 			}));
 
 			function makeFontFace(request) {
+				request.name = path.join(options.fontsDir, request.name);
 				return template
 					.replace(/\$(\w+)/g, function (m, name) { return request[name]; });
 			}
@@ -112,7 +154,7 @@ function getter(options) {
 
 				function emitFont(name, stream, next) {
 					self.push(new File({
-						path: options.fontsDir + name,
+						path: path.join(options.fontsDir, name),
 						contents: stream
 					}));
 					next();

.npmignore
@@ -1,4 +1,4 @@
-node_modules/
 example*/out/
+node_modules/
 .*.swp
 .*.swo

example1/.gitignore
@@ -1,0 +1,1 @@
+out/

example1/fonts.list
@@ -1,0 +1,5 @@
+# Tab-delimeted format
+Oswald	400,700	latin,latin-ext
+
+# Google format
+Roboto:500,500italic&subset=greek

example1/gulpfile.js
@@ -1,0 +1,17 @@
+var gulp = require('gulp');
+var googleWebFonts = require('../'); //require('gulp-google-webfonts');
+
+var options = {
+// fontsDir: 'googlefonts/',
+// cssDir: 'googlecss/',
+// cssFilename: 'myGoogleFonts.css'
+};
+
+gulp.task('fonts', function () {
+	return gulp.src('./fonts.list')
+		.pipe(googleWebFonts(options))
+		.pipe(gulp.dest('out/fonts'))
+		;
+	});
+
+gulp.task('default', ['fonts']);

example2/.gitignore
@@ -1,0 +1,1 @@
+out/

example2/fonts.list
@@ -1,0 +1,5 @@
+# Tab-delimeted format
+Oswald	400,700	latin,latin-ext
+
+# Google format
+Roboto:500,500italic&subset=greek

example2/gulpfile.js
@@ -1,0 +1,17 @@
+var gulp = require('gulp');
+var googleWebFonts = require('../'); //require('gulp-google-webfonts');
+
+var options = {
+	fontsDir: 'googlefonts/',
+	cssDir: 'googlecss/',
+	cssFilename: 'myGoogleFonts.css'
+};
+
+gulp.task('fonts', function () {
+	return gulp.src('./fonts.list')
+		.pipe(googleWebFonts(options))
+		.pipe(gulp.dest('out/fonts'))
+		;
+	});
+
+gulp.task('default', ['fonts']);

commit 1457c6f21f004d49719017ec38109ecb00f6fded

Fix bug that duplicated custom font directory
Fixes a bug introduced by b3f9818d292b906da60ceebcc161582c119c6ae8 that duplicated the custom font directory. The directory was added the first time [here](https://github.com/battlesnake/gulp-google-webfonts/blob/b3f9818d292b906da60ceebcc161582c119c6ae8/index.js#L137) and then again [here](https://github.com/battlesnake/gulp-google-webfonts/blob/b3f9818d292b906da60ceebcc161582c119c6ae8/index.js#L157).

index.js
@@ -154,7 +154,7 @@ function getter(options) {
 
 				function emitFont(name, stream, next) {
 					self.push(new File({
-						path: path.join(options.fontsDir, name),
+						path: name,
 						contents: stream
 					}));
 					next();