48ef3093e52795934b4c80c8c16a63c0d2700275
Mo <Radiergummi@users.noreply.github.com>
2018-05-11 15:57:32 +0200

    Create gulpfile.js

---
+++ example3/gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var googleWebFonts = require('gulp-google-webfonts');
+
+var options = {
+	fontsDir: 'googlefonts/',
+	cssDir: 'googlecss/',
+	cssFilename: 'myGoogleFonts.css',
+  relativePaths: true
+};
+
+gulp.task('fonts', function () {
+	return gulp.src('./fonts.list')
+		.pipe(googleWebFonts(options))
+		.pipe(gulp.dest('out/fonts'))
+		;
+	});
+
+gulp.task('default', ['fonts']);
